7e54093e69a027d9dd8226f5115e71af488378b3
Alberto <thebertsinfo@gmail.com>
2021-03-09 13:50:30 +0100

    Dinamic Slider / Update Color Hex

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll('.color h2');
 let initialColors;
 
+
+//Event Listeners
+sliders.forEach(slider => {
+    slider.addEventListener("input", getHslControls);
+});
+
+colorDivs.forEach((slider,index) => {
+    slider.addEventListener('change', ()=> {
+        updateTextUi(index);
+    });
+});
+
 //Functions
 
 //Hex Color Generator with Chroma JS library
@@ -49,6 +61,29 @@ function getColorSlider(color, hue, brightness, saturation) {
     saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(0)}, ${scaleSat(1)})`;
     brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBright(0)}, ${scaleBright(0.5)}, ${scaleBright(1)})`;
     hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75), rgb(204,204,75), rgb(75,204,75), rgb(75,204,204), rgb(75,75,204), rgb(204,75,204), rgb(204,75,75))`;
+}
+
+function getHslControls(e) {
+    const index = e.target.getAttribute('data-bright') || e.target.getAttribute('data-sat') || e.target.getAttribute('data-hue');
+
+    let sliders = e.target.parentElement.querySelectorAll('input[type="range"]');
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    const bgColor = colorDivs[index].querySelector('h2').innerText;
+
+    let color = chroma(bgColor).set('hsl.s', saturation.value).set('hsl.l', brightness.value).set('hsl.h', hue.value);
+
+    colorDivs[index].style.backgroundColor = color;
+}
+
+function updateTextUi(index) {
+    const activeDiv = colorDivs[index];
+    const color = chroma(activeDiv.style.backgroundColor);
+    const textHex = activeDiv.querySelector('h2');
+    const icons = activeDiv.querySelectorAll('.controls button');
+    textHex.innerText = color.hex();
 }
 
 
